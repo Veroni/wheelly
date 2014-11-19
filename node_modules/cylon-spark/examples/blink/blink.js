b58dc743dc55abd70c3fc9f4c0f2b0ee8cf5fb02
Veronika Bitkina <veronika@zisel.ca>
2014-11-18 17:21:32 -0800

    setting up nodeJS leapJS grunt johnny-five bower spark and index page

---
+++ node_modules/cylon-spark/examples/blink/blink.js
@@ -0,0 +1,20 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connection: {
+    name: 'spark',
+    adaptor: 'spark',
+    accessToken: '[YOUR_ACCESS_TOKEN]',
+    deviceId: '[YOUR_DEVICE_ID]'
+  },
+
+  device: {
+    name: 'led',
+    driver: 'led',
+    pin: 'D7'
+  },
+
+  work: function(my) {
+    every((1).second(), my.led.toggle);
+  }
+}).start();
