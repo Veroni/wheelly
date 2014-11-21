4487f852c70d5a31a68c8f77f163b1238c805872
Veronika Bitkina <veronika@zisel.ca>
2014-11-21 03:18:36 -0800

    raspi-io and socket.io added; ws connection changed server.js

--- robotMovement.js
+++ robotMovement.js
@@ -1,30 +1,30 @@
-// var movement = {
-//   connection: null,
-//   commands: [],
-//   connect: function(){
-//     movement.connection = new WebSocket("ws://127.0.0.1:6666");
-//   },
-//   listen: function(){
-//     if (movement.connection){
-//       movement.connection.onclick = function(command) {
-//         var payload = JSON.parse(command.data);
-//         movement[payload.cmd](payload);
-//       };
-//     }
-//   },
-//   init: function(){
-//     movement.connect();
-//     movement.listen();
-//   },
-//   payload: function(cmd, param) {
-//     return {cmd: cmd, value: param};
-//   },
-//   showMovementControls: function(join) {
-//     var sendBtn = $("#connectWheelly");
-//     join.off('click').hide();
-//     sendBtn.add()
-//   }
-// };
+var movement = {
+  connection: null,
+  commands: [],
+  connect: function(){
+    movement.connection = new WebSocket("ws://127.0.0.1:6666");
+  },
+  listen: function(){
+    if (movement.connection){
+      movement.connection.onclick = function(command) {
+        var payload = JSON.parse(command.data);
+        movement[payload.cmd](payload);
+      };
+    }
+  },
+  init: function(){
+    movement.connect();
+    movement.listen();
+  },
+  payload: function(cmd, param) {
+    return {cmd: cmd, value: param};
+  },
+  showMovementControls: function(join) {
+    var sendBtn = $("#connectWheelly");
+    join.off('click').hide();
+    sendBtn.add()
+  }
+};
 
 // $(function){
 //     $('.connectWheelly').on('click', movement.init)
--- server.js
+++ server.js
@@ -16,40 +16,78 @@
 //   });
 
 // };
-var five = require('johnny-five'),  
-  board = new five.board();
-  // one element passed [pwm] for non-derectional Motor pin
-  var motorRight = new five.Pin(0);
-
-  var motorLeft = new five.Pin(1);
-
-  board.on("ready", function({
-
-    function moveForward() {
-      motorRight.write(0);
-      motorLeft.write(0);
-    },
-
-    function stopMoving() {
-      motorRight.write(1);
-      motorLeft.write(1);
-    },
-
-    function turnLeft() {
-      motorRight.write(1);
-      motorLeft.write(0);
-    },
-
-    function turnRight() {
-      motorRight.write(0);
-      motorLeft.write(1);
-    }
-  }));
 
 
-client.on('message', function(message) {
-  var payload = JSON.parse(message.data);
-  var commands = [moveForward, stopMoving, turnLeft, turnRight];
 
-  commands[payload.cmd]();
-})
+//Initialize express and server
+var express = require('express')
+var app = express()
+        ,server = require('http').createServer(app);
+
+//Access server through port 80
+server.listen(80);
+
+//Set '/public' as the static folder
+app.use(express.static(_dirname + '/public'));
+
+//Set index.html as the base file
+app.get('/', function (req, res) {
+        res.sendfile(_dirname + '/index.html')
+});
+
+//Link socket.io to the previosly created server
+var io = require('socket.io').listen(server);
+
+//When connected
+io.sockets.on('connection', function (socket){
+        //Send out message
+        socket.emit('wheelly connected', { data: 'Connected' });
+
+        //When received 'robot command' message from this connection
+        socket.on('robot command', function (data){
+                console.log(data);
+
+                //Update Arduino motor values if the received command is correct
+                var command = data.command;
+        });
+});
+
+var five = require('johnny-five'),
+        //Initialize connection to Arduino (will crash if none is attached)
+        board = new five.board();
+
+        //Initialize motors as pin 0(right) and 1(left)
+        var motorRight = new five.Pin(0);
+        var motorLeft = new five.Pin(1);
+
+        //When connection is ready
+        board.on("ready", function(){
+
+        function moveForward() {
+                motorRight.write(0);
+                motorLeft.write(1);
+        };
+
+        function stopMoving() {
+                motorRight.write(1);
+                motorLeft.write(1);
+        };
+
+        function turnLeft() {
+                motorRight.write(1);
+                motorLeft.write(0);
+        };
+
+        function turnRight() {
+                motorRight.write(0);
+                motorLeft.write(1);
+        };
+});
+
+
+// client.on('message', function(message) {
+//   var payload = JSON.parse(message.data);
+//   var commands = [moveForward, stopMoving, turnLeft, turnRight];
+
+//   commands[payload.cmd]();
+// })
